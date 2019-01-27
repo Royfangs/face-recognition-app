b5d76c4c776d2154e990c46e75865adcc1cde399
Royfangs <royfangs@gmail.com>
2019-01-27 14:27:17 +0800

    Updates

---
+++ precache-manifest.0406a369990735f1aa686911215e4665.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6de2bf8da835eb85162e",
+    "url": "/face-recognition-app/static/js/runtime~main.6de2bf8d.js"
+  },
+  {
+    "revision": "5940f3a2a7ba19062918",
+    "url": "/face-recognition-app/static/js/main.5940f3a2.chunk.js"
+  },
+  {
+    "revision": "a189246c1c0b37793434",
+    "url": "/face-recognition-app/static/js/1.a189246c.chunk.js"
+  },
+  {
+    "revision": "5940f3a2a7ba19062918",
+    "url": "/face-recognition-app/static/css/main.bac587f8.chunk.css"
+  },
+  {
+    "revision": "a189246c1c0b37793434",
+    "url": "/face-recognition-app/static/css/1.5ea603f0.chunk.css"
+  },
+  {
+    "revision": "57ba90b413ae6e060def36e955a4eab3",
+    "url": "/face-recognition-app/index.html"
+  }
+];
