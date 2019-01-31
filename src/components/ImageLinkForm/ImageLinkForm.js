e50c81b101b23b0efc27a0bf14c5b87526454c18
Royfangs <royfangs@gmail.com>
2019-01-31 17:08:34 +0800

    correct typo

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
       <p className="f3">
-        {'This Brain will detect faces in your pictures. Please git it a try'}
+        {'This Brain will detect faces in your pictures. Please give it a try'}
       </p>
       <div className="center">
         <div className="form center pa4 br3 shadow-5">
